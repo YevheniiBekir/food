3ae8926be7f3a741d46d60cc6b6117b5ce6df0a9
YevheniiBekir <116861806+YevheniiBekir@users.noreply.github.com>
2022-12-27 07:59:09 +0200

    Work with delehuvannya podiy, work with tab-menu, work with classList. Creation tab-functional in the site.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
 'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    const hideTabContent = () => {
+        tabsContent.forEach(item => {
+        item.style.display = 'none';
+        });
+        
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    };
+    const showTabContent = (i = 0) => {
+        tabsContent[i].style.display = 'block';
+
+        tabs[i].classList.add('tabheader__item_active');
+    };
+    
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (e) => {
+        tabs.forEach((item, i) => {
+        if(e.target.className === 'tabheader__item'){
+                hideTabContent();
+                showTabContent(i);
+            }
+        });
+    });
+    
+});
