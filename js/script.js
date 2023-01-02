b162f86e5de5719284d8777313b161f067f7df1c
YevheniiBekir <116861806+YevheniiBekir@users.noreply.github.com>
2023-01-02 17:17:32 +0200

    redaction modal window

--- js/script.js
+++ js/script.js
@@ -108,35 +108,50 @@ document.addEventListener('DOMContentLoaded', () => {
           modalWindow = document.querySelector('.modal'),
           modalCloseBtn = document.querySelector('[modal-close]');
 
-    function openModal(window) {
-        window.classList.add('show');
-        window.classList.remove('hide');
+    function openModal() {
+        modalWindow.classList.add('show');
+        modalWindow.classList.remove('hide');
         document.body.style.overflow = 'hidden';
+        clearInterval(timeOfModalOpen);
     }
-    function closeModal (window) {
-        window.classList.add('hide');
-        window.classList.remove('show');
+    function closeModal () {
+        modalWindow.classList.add('hide');
+        modalWindow.classList.remove('show');
         document.body.style.overflow = '';
     }
 
+
     modalTrigger.forEach(btn => {
         btn.addEventListener('click', () => {
-            openModal(modalWindow);
+            openModal();
         });
 
     });
     modalCloseBtn.addEventListener('click', () => {
-        closeModal(modalWindow);
+        closeModal();
     });
-    
     modalWindow.addEventListener('click', (e) => {
         if(e.target === modalWindow){
             closeModal(modalWindow);
         }
     });
+
+
     document.addEventListener('keydown', (e) => {
         if(e.code === 'Escape' && modalWindow.classList.contains('show')){
             closeModal(modalWindow);
         }
     });
+
+
+    const timeOfModalOpen = setTimeout(openModal, 5000);
+
+
+    function showModalByScroll(){
+        if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1){
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+    window.addEventListener('scroll', showModalByScroll);
 });
